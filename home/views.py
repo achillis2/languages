2746825821214d6ef9cc3f121991dce4cf2c19bd
Ding Li <dli@nexant.com>
2023-11-17 15:09:55 -0800

    add the language switch on navbar and the language support

--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.utils.translation import gettext as _
+from django.utils.translation import activate
 
 
 # Create your views here.
 def index(request):
-    return render(request, "home.html")
+    # activate("en")
+    text = _("this is some random text")
+    return render(request, "home.html", {"text": text})
