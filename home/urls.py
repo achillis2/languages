b27df76055394ba1e16a05e0d18d6ab934aac96c
Ding Li <dli@nexant.com>
2023-11-17 18:48:09 -0800

    update urls

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
-from django.views.i18n import set_language
 
 
 urlpatterns = [
     path("", views.index),
-    path("set_language/", set_language, name="set_language"),
+    
 ]
